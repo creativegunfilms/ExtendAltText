0811e6c7ef404cd4f39be39839ff1e52a4cb69cf
Amit Sen <46849293+amit-sen@users.noreply.github.com>
2023-03-27 23:44:05 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,11 @@ export default function RootLayout({ children }) {
       <html>
         <head>
           <title>Extend Alt Text</title>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="extend alt text" />
+          <meta property="og:description" content="Generate Multiple Alt Text, AI Powered" />
+          <meta property="og:site_name" content="Next-Alt-Text-Generator" />
+          <meta property="og:url" content="https://extend-alt-text.vercel.app/" />
           <meta property="og:image" content="https://extend-alt-text.vercel.app/ogimage.jpg" />
         </head>
         <body className={SpaceG.className}>
