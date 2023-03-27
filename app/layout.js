050c5faabb8c08564bf8a16c676d2affab213162
Amit Sen <46849293+amit-sen@users.noreply.github.com>
2023-03-27 21:41:08 +0530

    og image

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Space_Grotesk } from 'next/font/google';
-import'../styles/globals.css';
+import '../styles/globals.css';
 
 const SpaceG = Space_Grotesk({ subsets: ['latin'] });
 
@@ -10,7 +10,10 @@ export default function RootLayout({ children }) {
       <html>
         <head>
           <title>Extend Alt Text</title>
-        
+          <meta
+            property="og:image"
+            content="https://extend-alt-text.vercel.app/ogimage"
+          />
         </head>
         <body className={SpaceG.className}>
           {children}
