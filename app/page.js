b89e37b509437ea6f26cd94f81ede764c4926817
Amit Sen <46849293+amit-sen@users.noreply.github.com>
2023-03-28 17:51:08 +0530

    update hidden og link

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default async function Page() {
       <section className={Styles.section}>
         <Multiple />
       </section>
-      <footer className={Styles.footer}>Developed by: <Link href={'https://extend-alt-text.vercel.app/ogimage'}>🔵🔴🟢</Link>  | India | 2023</footer>
+      <footer className={Styles.footer}>Developed by: <Link href={`https://extend-alt-text.vercel.app/ogimage?=${Math.floor(Math.random() * 1083)}`}>🔵🔴🟢</Link>  | India | 2023</footer>
     </div>
   );
 }
