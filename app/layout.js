f7cbb014d1eacdb127ca222befbdc0da881a8998
Amit Sen <46849293+amit-sen@users.noreply.github.com>
2023-03-28 23:38:45 +0530

    r noy update

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,12 @@ export default function RootLayout({ children }) {
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Extend alt text" />
           <meta property="og:description" content="Generate Multiple Alt Text, AI Powered" />
-          <meta property="og:site_name" content="Next-Alt-Text-Generator" />
+          <meta property="og:site_name" content="Extend Alt Text" />
           <meta property="og:url" content="https://extend-alt-text.vercel.app/" />
           <meta property="og:image" content="https://extend-alt-text.vercel.app/ogimage" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </head>
         <body className={SpaceG.className}>
           {children}
