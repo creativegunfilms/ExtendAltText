5c3054e34ec8f4c791e62491c9752b10368d9b6a
TITAS MALLICK <titasmallick@gmail.com>
2023-03-28 16:49:50 +0530

    Update route.js

--- app/ogimage/route.js
+++ app/ogimage/route.js
@@ -15,7 +15,7 @@ export async function GET(request) {
     let fetchDesc;
     try {
      const res = await fetch(`https://alt-text-generator.vercel.app/api/generate?imageUrl=${URL}`,
-        { mode: 'no-cors', next: { revalidate: true } },
+        { mode: 'no-cors', next: { revalidate: 10 } },
       );
       fetchDesc = await res.json();
     } catch (error) {
